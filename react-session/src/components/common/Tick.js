5648e155ca745dcaad6dbff8c59d2cd222296cbc
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-09-23 12:00:08 +0530

    used moment to format date

--- react-session/src/components/common/Tick.js
+++ react-session/src/components/common/Tick.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import moment from "moment";
 
 const Tick=()=> {
     const [currentTime, setCurrentTime] = useState('--:--:-- --')
     setInterval(() => {
-        const time = new Date();
-        const timeVal = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric',second:'numeric', hour12: true }).toString();
+        // const timeVal = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric',second:'numeric', hour12: true }).toString();
+        const timeVal = moment(new Date()).format('hh:mm:ss a');
         setCurrentTime(timeVal);
     }, 500);
     return (
