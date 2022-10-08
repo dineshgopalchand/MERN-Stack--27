7ef39976823cdb30d8693ff137cd48c1e38a7555
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-10-08 08:19:22 +0530

    React CSS module introduced

--- react-session/src/components/pages/contact/ContactForm.js
+++ react-session/src/components/pages/contact/ContactForm.js
@@ -1,6 +1,6 @@
 import react, { useState } from 'react';
-import { FormInput, FormResetButton, FormSubmitButton } from '../../UI/FormElements';
-// import './ContactForm.css';
+// import { FormInput, FormResetButton, FormSubmitButton } from '../../UI/FormElements';
+import formStyle from './ContactForm.module.css';
 
 const ContactForm = () => {
     const [formInput, setFormInput] = useState({
@@ -33,28 +33,51 @@ const ContactForm = () => {
 
     }
     return (
-        <form className='mt-3' mt={3}>
-            <FormInput  danger={formInput.uname?.error}>
-                <label htmlFor="uname" >Enter Your Name</label>
-                <input type="text" id="uname" name="uname" className="form-field" placeholder="Enter your name" onChange={formInputHandler} />
-            </FormInput>
-            <FormInput danger={formInput.email?.error}>
-                <label htmlFor="email">Enter Your Email</label>
-                <input type="email" id="email" name="email" className="form-field" placeholder="Enter your email" onChange={formInputHandler} />
-            </FormInput>
-            <FormInput danger={formInput.phone?.error}>
-                <label htmlFor="phone">Enter Your Contact Number</label>
-                <input type="tel" id="phone" name="phone" className="form-field" placeholder="Enter your contact number" onChange={formInputHandler} />
-            </FormInput>
-            <FormInput danger={formInput.subject?.error}>
-                <label htmlFor="subject">Enter Subject</label>
-                <input type="text" id="subject" name="subject" className="form-field" placeholder="Enter your subject" onChange={formInputHandler} />
-            </FormInput>
-            <FormInput>
-                <FormSubmitButton type="submit" >Submit</FormSubmitButton>
-                <FormResetButton>Reset</FormResetButton>
-            </FormInput>
-        </form>
+        <>
+            {/* <form className='mt-3' mt={3}>
+                <FormInput danger={formInput.uname?.error}>
+                    <label htmlFor="uname" >Enter Your Name</label>
+                    <input type="text" id="uname" name="uname" className="form-field" placeholder="Enter your name" onChange={formInputHandler} />
+                </FormInput>
+                <FormInput danger={formInput.email?.error}>
+                    <label htmlFor="email">Enter Your Email</label>
+                    <input type="email" id="email" name="email" className="form-field" placeholder="Enter your email" onChange={formInputHandler} />
+                </FormInput>
+                <FormInput danger={formInput.phone?.error}>
+                    <label htmlFor="phone">Enter Your Contact Number</label>
+                    <input type="tel" id="phone" name="phone" className="form-field" placeholder="Enter your contact number" onChange={formInputHandler} />
+                </FormInput>
+                <FormInput danger={formInput.subject?.error}>
+                    <label htmlFor="subject">Enter Subject</label>
+                    <input type="text" id="subject" name="subject" className="form-field" placeholder="Enter your subject" onChange={formInputHandler} />
+                </FormInput>
+                <FormInput>
+                    <FormSubmitButton type="submit" >Submit</FormSubmitButton>
+                    <FormResetButton>Reset</FormResetButton>
+                </FormInput>
+            </form> */}
+            <form className='mt-3' >
+                <div className={`${formStyle['form-element']} ${formInput.uname?.error ? formStyle.invalid : ''}`}>
+                    <label htmlFor="uname" >Enter Your Name</label>
+                    <input type="text" id="uname" name="uname" className={`${formStyle['form-field']}`} placeholder="Enter your name" onChange={formInputHandler} />
+                </div>
+                <div className={`${formStyle['form-element']} ${formInput.email?.error ? formStyle.invalid : ''}`}>
+                    <label htmlFor="email" >Enter Your Email</label>
+                    <input type="email" id="email" name="email" className={`${formStyle['form-field']}`} placeholder="Enter your email" onChange={formInputHandler} />
+                </div>
+                <div className={`${formStyle['form-element']} ${formInput.phone?.error ? formStyle.invalid : ''}`}>
+                    <label htmlFor="phone" >Enter Your Contact Number</label>
+                    <input type="tel" id="phone" name="phone" className={`${formStyle['form-field']}`} placeholder="Enter your contact number" onChange={formInputHandler} />
+                </div>
+                <div className={`${formStyle['form-element']} ${formInput.subject?.error ? formStyle.invalid : ''}`}>
+                    <label htmlFor="subject" >Enter Subject</label>
+                    <input type="text" id="subject" name="subject" className={`${formStyle['form-field']}`} placeholder="Enter your subject" onChange={formInputHandler} />
+                </div>
+                <div>
+                    <button className={`${formStyle.button}`} type="submit" >Submit</button>
+                </div>
+            </form>
+        </>
     )
 };
 export default ContactForm;
