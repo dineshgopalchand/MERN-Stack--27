f781824c2c646a5dee06bb7a00b3d997abcd51a5
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-10-12 08:46:00 +0530

    Discussed useEffect hook

--- react-session/src/App.js
+++ react-session/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
 import Contact from "./components/pages/contact/Contact";
 import Home from "./components/pages/home/Home";
+import Post from "./components/pages/post/Post";
 import TodoList from "./components/pages/todoList/TodoList";
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
       <Card.Body className="p-0">
         {/* <Home /> */}
         {/* <TodoList /> */}
-        <Contact />
+        {/* <Contact /> */}
+        <Post />
       </Card.Body>
       <Card.Footer className="p-0 mt-3">
         <Footer />
---
+++ react-session/src/components/pages/post/Post.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+import { Container } from 'react-bootstrap'
+
+const Post = () => {
+    const [posts, setPosts] = useState([]);
+
+    // it will get triggered on each re-rendering
+    useEffect(() => {
+        console.log('form useEffect with no deps');
+    });
+
+    useEffect(() => {
+        console.log('form useEffect with empty array as deps');
+        fetch('http://localhost:3030/posts')
+            .then(res => res.json())
+            .then(val => {
+                console.log(val);
+                setPosts(val);
+            });
+
+    }, []);
+
+    useEffect(() => {
+        console.log('form useEffect with post  as deps');
+    }, [posts]);
+
+    console.log('from outer');
+
+
+
+    return (
+        <>
+            <Container>
+                <h3>Posts ({`${posts.length}`})</h3>
+                {posts.map(post => {
+                    return (
+                        <div key={post.id} className="post border border-2 p-2 mb-2">
+                            <h4 >{posts[0].title}</h4>
+                            <div className="">{posts[0].body}</div>
+                        </div>
+                    );
+                })}
+
+            </Container>
+        </>
+    )
+}
+
+export default Post
