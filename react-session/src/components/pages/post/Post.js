33344b509f71bed56e70feef658c84de1da4fca0
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-10-19 08:39:31 +0530

    useEffect with mount,update,unmount ccompleted

--- react-session/src/components/pages/post/Post.js
+++ react-session/src/components/pages/post/Post.js
@@ -10,38 +10,36 @@ const Post = () => {
     const [totalRecord, setTotalRecord] = useState(30);
     const [pageNo, setPageNo] = useState(1);
     const [limit, setLimit] = useState(10);
-    const [paginationList, setPaginationList] = useState([1, 2, 3]);
 
     // it will get triggered on each re-rendering
-    useEffect(() => {
-        console.log('form useEffect with no deps');
-    });
+    // useEffect(() => {
+    //     console.log('form useEffect with no deps');
+    // });
 
     useEffect(() => {
-        // console.log('form useEffect with empty array as deps');
-        // axios.get(`http://localhost:3030/posts`)
-        //     .then(res => {
-        //         console.log(res.data);
-        //         setPosts(res.data);
-        //     }).catch(err => {
-        //         console.log({ err });
-        //     }).finally(() => {
-        //         console.log('finally get called');
-        //     });
+        // window.localStorage.setItem(`mounted`,true);
+
+        return () => {
+            console.log('unmounted');
+            window.localStorage.setItem(`mounted`,false);
+        }
 
     }, []);
 
     useEffect(() => {
         axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${pageNo}`)
-        // axios.get(`http://localhost:3030/posts?_limit=${limit}&_page=${pageNo}`)
             .then(res => {
                 setTotalRecord(res.headers['x-total-count']);
                 setPosts(res.data);
+                window.localStorage.setItem(`post-data-${limit}-${pageNo}`,JSON.stringify(res.data));
             }).catch(err => {
                 console.log({ err });
-            }).finally(() => {
-                console.log('finally get called');
-            });
+            })
+        return () => {
+            console.log('unmounted1', limit, pageNo);
+            window.localStorage.removeItem(`post-data-${limit}-${pageNo}`);
+
+        }
     }, [limit, pageNo]);
 
     const paginationCick = (pageNo) => {
@@ -51,12 +49,12 @@ const Post = () => {
     return (
         <>
             <Container>
-                <h3>Posts ({`${posts.length}`})</h3>
+                <h3>Posts <span style={{ fontSize: '0.5em' }}>({`Total: ${totalRecord}, item: ${((pageNo - 1) * limit) + 1} - ${(totalRecord < pageNo * limit) ? totalRecord : (pageNo * limit)}`})</span></h3>
                 {posts.map(post => {
                     return (
                         <div key={post.id} className="post border border-2 p-2 mb-2">
-                            <h4 >{posts[0].title}</h4>
-                            <div className="">{posts[0].body}</div>
+                            <h4 >{post.title}</h4>
+                            <div className="">{post.body}</div>
                         </div>
                     );
                 })}
