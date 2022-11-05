0aa7ecea00c1e249c507d0b9e58ecad18494e6e7
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-11-05 10:07:00 +0530

    Added Thanks message in contact form

--- react-session/src/components/pages/contact-form/ContactForm.js
+++ react-session/src/components/pages/contact-form/ContactForm.js
@@ -35,6 +35,7 @@ const ContactForm = () => {
     const [formVal, setFormVal] = useState(initFormValue);
     const [formErrors, setFormErrors] = useState({});
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const [formSubmitSuccess, setFormSubmitStatus] = useState(false);
 
     const formSubmitHandler = e => {
         e.preventDefault();
@@ -44,6 +45,9 @@ const ContactForm = () => {
 
     const submitForm = () => {
         console.log(formVal);
+        setTimeout(()=>{
+            setFormSubmitStatus(true);
+        },3000);
     }
     const formFieldhandler = e => {
         const value = e.target.value;
@@ -63,57 +67,63 @@ const ContactForm = () => {
             <div className="row my-5 ">
                 <div className='col'>
                     <div className='card p-3 m-2 '>
-                        <form onSubmit={formSubmitHandler}>
-                            <div className={`form-group ${formErrors.fullname ? classes.errorfield : ''}`}>
-                                <label htmlFor="fullname">Full Name
-                                    {
-                                        formErrors.fullname &&
-                                        <span className={classes.errordetails}> ( {formErrors.fullname} ) </span>
-                                    }
-                                </label>
-                                <input type="text" name="fullname" value={formVal.fullname} className="form-control" id="fullname" placeholder='Enter full name' onChange={formFieldhandler} />
+                        {formSubmitSuccess ? (
+                            <div className="alert alert-primary">Thank you for contacting us</div>
+                        ) :
+                            (
+                                <form onSubmit={formSubmitHandler}>
+                                    <div className={`form-group ${formErrors.fullname ? classes.errorfield : ''}`}>
+                                        <label htmlFor="fullname">Full Name
+                                            {
+                                                formErrors.fullname &&
+                                                <span className={classes.errordetails}> ( {formErrors.fullname} ) </span>
+                                            }
+                                        </label>
+                                        <input type="text" name="fullname" value={formVal.fullname} className="form-control" id="fullname" placeholder='Enter full name' onChange={formFieldhandler} />
 
-                            </div>
-                            <div className={`form-group ${formErrors.email ? classes.errorfield : ''}`}>
-                                <label htmlFor="email">Email
-                                    {
-                                        formErrors.email &&
-                                        <span className={classes.errordetails}> ( {formErrors.email} ) </span>
-                                    }
-                                </label>
-                                <input type="email" name="email" value={formVal.email} className="form-control" id="email" placeholder='Enter email' onChange={formFieldhandler} />
-                            </div>
-                            <div className={`form-group ${formErrors.contact ? classes.errorfield : ''}`}>
-                                <label htmlFor="contact">Contact Number
-                                    {
-                                        formErrors.contact &&
-                                        <span className={classes.errordetails}> ( {formErrors.contact} ) </span>
-                                    }
-                                </label>
-                                <input type="tel" name="contact" value={formVal.contact} className="form-control" id="contact" onChange={formFieldhandler} />
-                            </div>
-                            <div className={`form-group ${formErrors.subject ? classes.errorfield : ''}`}>
-                                <label htmlFor="subject">Subject
-                                    {
-                                        formErrors.subject &&
-                                        <span className={classes.errordetails}> ( {formErrors.subject} ) </span>
-                                    }
-                                </label>
-                                <input type="text" name="subject" className="form-control" value={formVal.subject} id="subject" placeholder='Enter subject' onChange={formFieldhandler} />
-                            </div>
-                            <div className={`form-group ${formErrors.details ? classes.errorfield : ''}`}>
-                                <label htmlFor="details">Description
-                                    {
-                                        formErrors.details &&
-                                        <span className={classes.errordetails}> ( {formErrors.details} ) </span>
-                                    }
-                                </label>
-                                <textarea name="details" id="details" rows="5" className="form-control" placeholder='Please mention full details' value={formVal.details} onChange={formFieldhandler}></textarea>
-                            </div>
-                            <div className="form-group m-2">
-                                <button className="btn btn-primary form-control" disabled={isSubmitting}>Submit</button>
-                            </div>
-                        </form>
+                                    </div>
+                                    <div className={`form-group ${formErrors.email ? classes.errorfield : ''}`}>
+                                        <label htmlFor="email">Email
+                                            {
+                                                formErrors.email &&
+                                                <span className={classes.errordetails}> ( {formErrors.email} ) </span>
+                                            }
+                                        </label>
+                                        <input type="email" name="email" value={formVal.email} className="form-control" id="email" placeholder='Enter email' onChange={formFieldhandler} />
+                                    </div>
+                                    <div className={`form-group ${formErrors.contact ? classes.errorfield : ''}`}>
+                                        <label htmlFor="contact">Contact Number
+                                            {
+                                                formErrors.contact &&
+                                                <span className={classes.errordetails}> ( {formErrors.contact} ) </span>
+                                            }
+                                        </label>
+                                        <input type="tel" name="contact" value={formVal.contact} className="form-control" id="contact" onChange={formFieldhandler} />
+                                    </div>
+                                    <div className={`form-group ${formErrors.subject ? classes.errorfield : ''}`}>
+                                        <label htmlFor="subject">Subject
+                                            {
+                                                formErrors.subject &&
+                                                <span className={classes.errordetails}> ( {formErrors.subject} ) </span>
+                                            }
+                                        </label>
+                                        <input type="text" name="subject" className="form-control" value={formVal.subject} id="subject" placeholder='Enter subject' onChange={formFieldhandler} />
+                                    </div>
+                                    <div className={`form-group ${formErrors.details ? classes.errorfield : ''}`}>
+                                        <label htmlFor="details">Description
+                                            {
+                                                formErrors.details &&
+                                                <span className={classes.errordetails}> ( {formErrors.details} ) </span>
+                                            }
+                                        </label>
+                                        <textarea name="details" id="details" rows="5" className="form-control" placeholder='Please mention full details' value={formVal.details} onChange={formFieldhandler}></textarea>
+                                    </div>
+                                    <div className="form-group m-2">
+                                        <button className="btn btn-primary form-control" disabled={isSubmitting}>Submit</button>
+                                    </div>
+                                </form>
+                            )
+                        }
                     </div>
                 </div>
                 <div className="col  m-2">
@@ -122,7 +132,7 @@ const ContactForm = () => {
                     <img className={classes['contact-image']} src={treeImage} alt="natural scene" />
                 </div>
             </div>
-        </div>
+        </div >
 
     )
 }
