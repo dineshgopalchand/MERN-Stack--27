7478abe8a3757a69ba76f6f04fafe699c34312cf
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-11-17 08:55:14 +0530

    Config router url

--- router-concept/src/App.js
+++ router-concept/src/App.js
@@ -6,17 +6,45 @@ import '@fontsource/roboto/700.css';
 import Footer from './layout/Footer';
 import Header from './layout/Header';
 import Home from './pages/Home';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "home",
+      element: <Home />,
+    },
+    {
+      path: "services",
+      element: <Services />,
+    },
+    {
+      path: "contact",
+      element: <Contact />,
+    },
+    {
+      path: "about",
+      element: <About />,
+    },
+  ]);
   return (
-    <div className="App">
-      <Header />
-      <main className='main-container'>
-        <Home />
-      </main>
-      <Footer />
-    </div>
+    <RouterProvider router={router}>
+      <div className="App">
+        <Header />
+        <main className='main-container'>
+          <Home />
+        </main>
+        <Footer />
+      </div>
+    </RouterProvider>
   );
 }
 
