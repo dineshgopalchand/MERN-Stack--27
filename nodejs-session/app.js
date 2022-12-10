a0ddfd3cfb1abe5649afa18d7838d896ea3f91fe
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-12-10 12:40:04 +0530

    Use FS module read file

--- nodejs-session/app.js
+++ nodejs-session/app.js
@@ -1,8 +1,13 @@
 const http=require('http');
+var fs = require('fs');
 // console.log(http);
 http.createServer((req,res)=>{
-    console.log(req);
-    res.setHeader('status',200);
-    res.setHeader('content-type','text/plain');
-    res.end('first node server response')
+    // console.log(req);
+    fs.readFile('javascript.txt', 'utf8', function(err, data) {
+        if (err) throw err;
+        res.setHeader('status',200);
+        res.setHeader('content-type','text/plain');
+        res.end(data)
+      });
+
 }).listen(8888);
