cf56bb224c119c41726b78ec725bc82965fbff5e
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-12-13 09:05:24 +0530

    created Simple server using http.createServer

--- nodejs-session/app.js
+++ nodejs-session/app.js
@@ -1,13 +1,33 @@
-const http=require('http');
+const http = require('http');
 var fs = require('fs');
-// console.log(http);
-http.createServer((req,res)=>{
-    // console.log(req);
-    fs.readFile('javascript.txt', 'utf8', function(err, data) {
-        if (err) throw err;
-        res.setHeader('status',200);
-        res.setHeader('content-type','text/plain');
-        res.end(data)
-      });
+const { uuid } = require('uuidv4');
+const url=require('url');
+console.log(uuid());
 
-}).listen(8888);
+const port = process.env.port || 8000;
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  // const urlVal =new URL ('http://localhost:8000'+req.url);
+  const urlVal =url.parse(req.url);
+  const pathname=urlVal.pathname;
+  if(pathname=='/'){
+    res.setHeader('status', 200);
+    res.setHeader('content-type', 'text/plain');
+    return res.end('Home page');
+  }else if(pathname=='/about'){
+    res.setHeader('status', 200);
+    res.setHeader('content-type', 'text/plain');
+    return res.end('About');
+  }else if(pathname=='/contact'){
+    res.setHeader('status', 200);
+    res.setHeader('content-type', 'text/plain');
+    return res.end('Contact');
+  }
+  res.setHeader('status', 200);
+  res.setHeader('content-type', 'text/plain');
+  res.end('Not Found');
+});
+server.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
